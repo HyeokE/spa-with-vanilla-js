43484c3296b8308dd89aa718d45cb295dc410e87
HYEOK_E <jhjeong00@gmail.com>
2022-02-27 01:02:30 +0900

    feat: route setting

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const app = express();
 
-app.use("/routes", express.static(path.resolve( "src", "routes")));
+app.use("/routes", express.static(path.resolve( __dirname, "src", "routes")));
 
 
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("src", "index.html"));
+    res.sendFile(path.resolve(__dirname, "src", "index.html"));
 });
 
 app.listen(process.env.PORT || 3030, () => console.log("Server running..."));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,21 @@
-// const router = async () => {
-//     const routes = [
-//         { path : '/', view: ()=> console.log('home')},
-//         { path : '/introduce', view: ()=> console.log('introduce')},
-//         { path : '/faq', view: ()=> console.log('faq')},
-//     ];
-//     const pageCheck = routes.map((route)=>{
-//         return {
-//             route: route,
-//             isMatch: route.path === location.pathname
-//         }
-//     });
-//     console.log(pageCheck());
-// }
-// router();
+const router = () => {
+    const routes = [
+        {path: '/', view: () => console.log('home')},
+        {path: '/introduce', view: () => console.log('introduce')},
+        {path: '/faq', view: () => console.log('faq')},
+    ];
+    const pageCheck = routes.map((route) => {
+        return {
+            route: route,
+            isMatch: route.path === location.pathname
+        }
+    });
+    const matches = pageCheck.filter((page) => page.isMatch);
+    if(matches.length < 1) {
+        document.querySelector('#app').innerHTML = '<h1>404</h1>';
+    }
+    console.log(...matches.route);
+}
+router();
 
-console.log('hello');
-console.log(__dirname);
+console.log(location.pathname)
